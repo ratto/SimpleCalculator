76b96a420ed4ded2dcdf8f91b0ec2b7e422e3dad
Pedro Luiz Tosta da Paixão <rattopedro@gmail.com>
2019-02-22 22:20:59 -0300

    First commit

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+/*
+ * This is a simple calculator exercise to workout C# skills.
+ * In this version I just wanted to make a first commit to keep
+ * on working. For now I will keep the code simple and silly, but
+ * I will enhance this code later. My worry for now is on leaving
+ * this first version working properly.
+ */
+
+namespace SimpleCalculator {
+    class Program {
+        static void Main(string[] args) {
+            double result = 0;
+            double number;
+            String oper;
+
+            Console.WriteLine("Simple Calculator");
+            number = double.Parse(Console.ReadLine());
+            oper = Console.ReadLine();
+
+            // For now is just inputing one number, hitting enter and so on
+            if (oper == "+") {
+                result = SimpleOperators.Sum(number);
+            } else if(oper == "-") {
+                result = SimpleOperators.Subtract(number);
+            } else if (oper == "*" || oper == "x") {
+                result = SimpleOperators.Multiply(number);
+            } else if (oper == "/") {
+                result = SimpleOperators.Divide(number);
+            } else {
+                Console.WriteLine("Error");
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleOperators.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+/* This class will provide the methods for the 4 basic 
+ * operations
+ */
+
+namespace SimpleCalculator {
+    class SimpleOperators {
+        static private double Number;
+
+        static public double Sum(double n) {
+            Number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            return (n) + (Number);
+        }
+
+        static public double Subtract(double n) {
+            Number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            return (n) - (Number);
+        }
+
+        static public double Multiply(double n) {
+            Number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            return (n) * (Number);
+        }
+
+        static public double Divide(double n) {
+            Number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            return (n) / (Number);
+        }
+    }
+}
